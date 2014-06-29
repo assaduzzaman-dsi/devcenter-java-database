4311d7cc0a720ca286f6a6eeb4a0a43963218f08
assad <assadice@yahoo.com>
2014-06-29 16:18:38 +0600

    solve porblem of ssl

--- devcenter-java-database-plain-jdbc/src/main/java/com/heroku/example/Main.java
+++ devcenter-java-database-plain-jdbc/src/main/java/com/heroku/example/Main.java
@@ -21,11 +21,15 @@ public class Main {
     }
     
     private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+    	
+    	String url = "postgres://webmxepepayoov:IVWp0871dGYbbMyqrOVUv0eNeb@ec2-54-197-250-40.compute-1.amazonaws.com:5432/d22j8uu3jhe3qq";
+        URI dbUri = new URI(url);
 
+//        System.get
+        
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath() + "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
 
         return DriverManager.getConnection(dbUrl, username, password);
     }
